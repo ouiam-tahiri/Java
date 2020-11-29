bfc4d679531b77b0f8d446673148f1f11f774e05
ouiam-tahiri <74799369+ouiam-tahiri@users.noreply.github.com>
2020-11-29 13:37:28 +0100

    Create Incrementation.java

---
+++ Incrementation.java
@@ -0,0 +1,14 @@
+Incrémentation de classe  publique {
+    public  static  void  main ( String [] args ) {
+        int n;
+        n = 10 ;
+        Système . dehors . println ( " n \ n " + n ++ );
+        Système . dehors . println (n);
+        Système . dehors . println ( ++ n);
+        int a = 5 ;
+        Système . dehors . println ( " a \ n " + (a + = 1 ));
+        Système . dehors . println ((a - = 1 ));
+
+    }
+
+}
