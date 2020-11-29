049ea467c73e335a55cd01043dbae693626a0ee3
ouiam-tahiri <74799369+ouiam-tahiri@users.noreply.github.com>
2020-11-29 13:42:46 +0100

    Create Premfen.java

---
+++ Premfen.java
@@ -0,0 +1,9 @@
+import  javax.swing. * ;
+ classe  publique Premfen
+{ public  static  void  main ( String  args [])
+{ JFrame fen =  nouveau  JFrame ();
+fen . setSize ( 300 , 150 );
+fen . setTitle ( " Ma premiere fenetre " );
+fen . setVisible ( vrai );
+}
+}
