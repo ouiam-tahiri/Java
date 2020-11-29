097120b2151a9ca45fc7201dba7218f619ed89ec
ouiam-tahiri <74799369+ouiam-tahiri@users.noreply.github.com>
2020-11-29 13:45:48 +0100

    Create Tableu_de_mult.java

---
+++ Tableu_de_mult.java
@@ -0,0 +1,21 @@
+public  class  Tableu_de_mult {
+
+    public  static  void  main ( String [] args ) {
+        // int comp = 0;
+        pour ( int i = 20 ; i > = 0 ; i - ) {
+            si ((i / 10 == 0 ) && (i > = 0 ))
+            Système . dehors . impression ( "   "  + i + "   " );
+            sinon  si ((i / 10 == 0 ) && (i <= 0 ))
+            Système . dehors . impression ( "  " + i + "   " );
+            sinon  si ((i / 10 ! = 0 ) && (i <= 0 ))
+            Système . dehors . imprimer (i + "   " );
+            autre
+            Système . dehors . impression ( "  " + i + "   " );
+            si (i % 3 == 0 ) {
+                Système . dehors . println ();
+            }
+        }
+
+    }
+
+} 
