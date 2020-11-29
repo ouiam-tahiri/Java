7afbe5c3172808fd037c674339ca77935cadd684
ouiam-tahiri <74799369+ouiam-tahiri@users.noreply.github.com>
2020-11-29 13:44:43 +0100

    Create String_Nom.java

---
+++ String_Nom.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner ;
+
+public  class  String_Nom {
+
+    public  static  void  main ( String [] args ) {
+		String nom;
+        Scanner sc = nouveau  scanner ( System . In );
+        Système . dehors . println ( " taper votre nom " );
+        nom = sc . nextLine ();
+        Système . dehors . println ( " \ n Bienvenue " + nom);
+	}
+} 
