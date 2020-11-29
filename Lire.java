adfa80968d0f7a0355a6a6424a2467672a9ca3e1
ouiam-tahiri <74799369+ouiam-tahiri@users.noreply.github.com>
2020-11-29 13:38:37 +0100

    Create Lire.java

---
+++ Lire.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner ;
+public  class  Lire {
+    public  static  void  main ( String [] args ) {
+        String nom, prenom;
+        Scanner sc =  nouveau  scanner ( System . In );
+        Système . dehors . println ( " Entrez un nom SVP " );
+        nom = sc . nextLine ();
+        Système . dehors . println ( " Entrez un prenom SVP " );
+        prenom = sc . nextLine ();
+        Système . dehors . println ( " Votre nom est: " + nom);
+        Système . dehors . println ( " Votre prenom est: " + prenom);
+    }
+}
