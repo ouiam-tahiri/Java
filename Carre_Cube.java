859d76a534e37887fdef5c63d426b4a3c712df21
ouiam-tahiri <74799369+ouiam-tahiri@users.noreply.github.com>
2020-11-29 13:35:56 +0100

    Create Carre_Cube.java

---
+++ Carre_Cube.java
@@ -0,0 +1,15 @@
+import  java.util.Scanner ;
+
+ classe  publique Carre_Cube {
+
+    public  static  void  main ( String [] args ) {
+		int a, b, c = 0 ;
+		Scanner sc =  nouveau  scanner ( System . In );
+        Système . dehors . println ( " cône un nombre " );
+        a = sc . nextInt ();
+        b = a * a;
+        c = b * a;
+        Système . dehors . println ( " le carre de " + a + " est " + b);
+        Système . dehors . println ( " le cube de " + a + " est " + c);
+	}
+} 
